4dd6c5b9d525a38cc36f5f7e553211eadef1d0b7
Dannie <dwan2324@uni.sydney.edu.au>
2023-08-23 19:10:54 +1000

    docker development

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-nohup python3 /home/CS74-1-2023S1/backend/manage.py runserver 0.0.0.0:8000 >> /home/CS74-1-2023S1/run.log 2>&1 &
+nohup python3 /home/CS37-2-2023S2/backend/manage.py runserver 0.0.0.0:8000 >> /home/CS37-2-2023S2/run.log 2>&1 &
 service nginx restart
-tail -f /home/CS74-1-2023S1/run.log
+tail -f /home/CS37-2-2023S2/run.log
